e81b79ab56eb3760b733bac8356efe76d4ab8ba5
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-02-23 18:40:00 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // Set up routes
 router.get('/login', (req, res) => {
   res.render('login')
@@ -9,6 +11,30 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  console.log('ok')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('The email has already been used!')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    } else {
+      User.create({
+        name,
+        email,
+        password,
+      })
+        .then(() => res.render('/'))
+        .catch(error => console.log(error))
+    }
+  })
 })
 
 // Export module
